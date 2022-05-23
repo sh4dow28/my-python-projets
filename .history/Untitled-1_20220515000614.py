8d8f43d87660e38fb88c9ca03916f20627a371ac
Winx <86253045+sh4dow28@users.noreply.github.com>
2022-05-23 14:35:30 +0000

    Delete .history directory

--- .history/Untitled-1_20220515000614.py
+++
@@ -1,6 +0,0 @@
-
-
-a, b, c = 3, 2, 1
-while c < 15:
-    print(c, ": ", b)
-    a, b, c = b, a*b, c+1
--- .history/second_convert_time_20220514233039.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: UTF-8 -*-
-
-# Écrivez un programme qui convertit un nombre entier de secondes fourni au départ en un
-# nombre d’années, de mois, de jours, de minutes et de secondes (utilisez l’opérateur modulo : %
-# ).
-
-initNbSecond = int(input("Enter a number of seconds : "))
-
